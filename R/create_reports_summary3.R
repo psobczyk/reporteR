4ebd2cf64630a258463ee797eee25f17c9dc423b
michbur <michalburdukiewicz@gmail.com>
2015-05-13 11:51:30 +0200

    some problems solved, but still returns errors

--- R/create_reports_summary3.R
+++ R/create_reports_summary3.R
@@ -13,23 +13,22 @@ create_reports_summary3 <- function(directory, file_name = "toc") {
 #' @export
 create_files_table <- function(directory) {
   repo_files <- list.files(path = directory, recursive = TRUE)
-  
   sapply(repo_files, function(single_file) {
-    all_lines <- readLines(paste0(directory, single_file))
-    title <- pattern_lines("^title:")
-    abstract <- pattern_lines("^abstract:")
-    sources <- pattern_lines("^source\\(")
+    all_lines <- readLines(paste0(directory, "/", single_file))
+    title <- pattern_lines("^title:", all_lines)
+    abstract <- pattern_lines("^abstract:", all_lines)
+    sources <- pattern_lines("^source\\(", all_lines)
     c(title, abstract, sources)
   })
 }
-    
+
 
 
 pattern_lines <- function(pattern, all_lines) {
   source_lines <- grep(pattern, all_lines, fixed = FALSE)
   source_file_names <- if(length(source_lines) > 0) {
     vapply(all_lines[source_lines], function(single_line)
-      strsplit(strsplit(single_line, pattern, fixed = TRUE)[[1]][[2]],
+      strsplit(strsplit(single_line, pattern, fixed = FALSE)[[1]][[2]],
                '\"')[[1]][[1]], "a")
   } else {
     ""
